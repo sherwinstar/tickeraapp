55d965e5ea0d51e6ae17d8b7519ac13cb81e40ba
sherwinstar <slzhouzsl@163.com>
2022-04-06 23:59:12 +0800

    update activity

--- app/src/main/java/com/tickera/tickeraapp/HomeActivity.kt
+++ app/src/main/java/com/tickera/tickeraapp/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tickera.tickeraapp.stripe_terminal.StripeTerminalIntegrationActivity
+import com.tickera.tickeraapp.wedgescan.WedgeContinuousScanActivity
+import com.tickera.tickeraapp.wedgescan.WedgeOneScanActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -17,6 +19,14 @@ class HomeActivity : AppCompatActivity() {
 
         button2.setOnClickListener{
             startActivity(Intent(this, StripeTerminalIntegrationActivity::class.java))
+        }
+
+        button3.setOnClickListener{
+            startActivity(Intent(this,WedgeOneScanActivity::class.java))
+        }
+
+        button4.setOnClickListener{
+            startActivity(Intent(this, WedgeContinuousScanActivity::class.java))
         }
     }
 }
